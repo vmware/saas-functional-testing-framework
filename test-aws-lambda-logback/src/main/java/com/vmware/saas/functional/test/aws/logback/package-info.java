a100cd336bfb9035e2e1e9351169386ad65a45ac
Plamen Penev <ppenev@vmware.com>
2022-03-02 17:20:18 +0200

    Adding simple module to setup project

---
+++ test-aws-lambda-logback/src/main/java/com/vmware/saas/functional/test/aws/logback/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2022 VMware, Inc.
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+/**
+ * Contains logback infrastructure for lambda tests.
+ */
+package com.vmware.saas.functional.test.aws.logback;
