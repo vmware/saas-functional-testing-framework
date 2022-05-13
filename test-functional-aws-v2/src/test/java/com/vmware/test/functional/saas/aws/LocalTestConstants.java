37c5cf709244978fd719c97dd64249574834b3f8
Plamen Penev <ppenev@vmware.com>
2022-05-13 11:48:49 +0300

    Update kinesalite docker image
    Previously used docker image is no longer available.

--- test-functional-aws-v2/src/test/java/com/vmware/test/functional/saas/aws/LocalTestConstants.java
+++ test-functional-aws-v2/src/test/java/com/vmware/test/functional/saas/aws/LocalTestConstants.java
@@ -15,7 +15,7 @@ public final class LocalTestConstants {
     public static final String TEST_ACCESS_KEY_ID = "test_access_key_id";
     public static final String TEST_SECRET_KEY_ID = "test_secret_key_id";
 
-    public static final String KINESALITE_DOCKER_IMAGE = "countingup/kinesalite:latest";
+    public static final String KINESALITE_DOCKER_IMAGE = "instructure/kinesalite:latest";
     public static final int KINESALITE_PORT = 4567;
     public static final int KINESALITE_HOST_PORT = new PortSupplier().getAsInt();
 
