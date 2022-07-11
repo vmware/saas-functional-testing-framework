53fed7a547b9fa28d82fb0e3761e8183aad2c657
Borislav Tonchev <btonchev@vmware.com>
2022-07-11 12:25:28 +0300

    minor typo changes

--- test-functional/src/main/java/com/vmware/test/functional/saas/AbstractResourceCreator.java
+++ test-functional/src/main/java/com/vmware/test/functional/saas/AbstractResourceCreator.java
@@ -14,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Parent class for all AWS initializers.
+ * Parent class for all resource initializers.
  */
 @Slf4j
 @RequiredArgsConstructor
